afccc08a0551eeb6760c6d3c2af29a3ae194511f
Josh Hull <j.hull@kunzleigh.com>
2018-06-28 09:11:56 -0400

    MQE Rename

--- src/main/java/org/immregistries/mqe/hub/submission/NistValidatorHandler.java
+++ src/main/java/org/immregistries/mqe/hub/submission/NistValidatorHandler.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import org.immregistries.mqe.hl7util.Reportable;
 import org.immregistries.mqe.hub.cfg.MqeMessageHubApplicationProperties;
-import org.immregistries.mqe.nist.validator.connector.NISTValidator;
+import org.immregistries.nist.validator.connector.NISTValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
