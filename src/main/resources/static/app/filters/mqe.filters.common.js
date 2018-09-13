ac0af4fd44b69683da648850ac9e8960c7bef543
Josh Hull <j.hull@kunzleigh.com>
2018-09-13 14:52:20 -0400

    Pre-release Cleanup

--- src/main/resources/static/app/filters/mqe.filters.common.js
+++ src/main/resources/static/app/filters/mqe.filters.common.js
@@ -1,59 +1,58 @@
 angular
-	.module('messageHubDemoApp')
-	.filter("filterObjectToQueryString", function() {
-		return function(filterObject) {
+.module('messageHubDemoApp')
+.filter("filterObjectToQueryString", function () {
+  return function (filterObject) {
 //			console.log("filter object: ");
 //			console.log(filterObject);
-			var filtersString = '';
-			for (property in filterObject) {
-				if (angular.isNotUndefinedOrNullOrEmpty(filterObject[property])) {
+    var filtersString = '';
+    for (property in filterObject) {
+      if (angular.isNotUndefinedOrNullOrEmpty(filterObject[property])) {
 //					console.log("filter object property["+property+"] value[" + filterObject[property]+"]");
-					
-					if (filtersString === '') {
-						filtersString += property + '::' + filterObject[property];
-					} else {
-						filtersString += '|' + property + '::' + filterObject[property];
-					}
-					
-				}
-			}
-			return filtersString;
-		}
-	})
-	
-	.filter("filterObjectFromQueryString", function() {
-		return function(filterString) {
-			
-			if (typeof filterString != 'string') {
-				
-				
-				return filterString;
-			}
-			var filtersObject = {};
-			
-			if (angular.isNotUndefinedOrNullOrEmpty(filterString)) {
-			
-			var filterPairs = filterString.split("|");
-			
-			for (pair in filterPairs) {
-				
-				if (angular.isNotUndefinedOrNullOrEmpty(filterPairs[pair])) {
-					
-					var paramAndVal = filterPairs[pair].split("::");
-					
-					if (angular.isNotUndefinedOrNullOrEmpty(paramAndVal)) {
-						
-						var name = paramAndVal[0];
-						var value = paramAndVal[1];
-						filtersObject[name] = value;
-					}
-				}
-			}
-			}
-			return filtersObject;
-		}
-	})
-	
+
+        if (filtersString === '') {
+          filtersString += property + '::' + filterObject[property];
+        } else {
+          filtersString += '|' + property + '::' + filterObject[property];
+        }
+
+      }
+    }
+    return filtersString;
+  }
+})
+
+.filter("filterObjectFromQueryString", function () {
+  return function (filterString) {
+
+    if (typeof filterString != 'string') {
+
+      return filterString;
+    }
+    var filtersObject = {};
+
+    if (angular.isNotUndefinedOrNullOrEmpty(filterString)) {
+
+      var filterPairs = filterString.split("|");
+
+      for (pair in filterPairs) {
+
+        if (angular.isNotUndefinedOrNullOrEmpty(filterPairs[pair])) {
+
+          var paramAndVal = filterPairs[pair].split("::");
+
+          if (angular.isNotUndefinedOrNullOrEmpty(paramAndVal)) {
+
+            var name = paramAndVal[0];
+            var value = paramAndVal[1];
+            filtersObject[name] = value;
+          }
+        }
+      }
+    }
+    return filtersObject;
+  }
+})
+
 //	.filter("statNameLookup", function() {
 //		return function(statId) {
 //			
@@ -64,46 +63,79 @@ angular
 //			return statId;
 //		}
 //	})
-	
-	.filter('byRange', [function() {
-			return function(inputArray, fieldName, min, max){
-				var data=[];
-				angular.forEach(inputArray, function(item){    
+
+.filter('byRange', [function () {
+  return function (inputArray, fieldName, min, max) {
+    var data = [];
+    angular.forEach(inputArray, function (item) {
 //					console.log("field value: " + item[fieldName] + " invoking by range with " + fieldName + " min: " + min + " max: "+ max);
-					if(item[fieldName] > min && item[fieldName] < max){
-						data.push(item);
-					}
-				});      
-				return data;
-			};
-	}])
-	
-	.filter('immunizationStats', ['$filter', function($filter) {
-			return function(inputArray){ 
-				return $filter('byRange')(inputArray, 'statisticId', 20,29);
-			};
-	}])
-	
-	.filter('respPartyStats', ['$filter', function($filter) {
-		return function(inputArray){ 
-			return $filter('byRange')(inputArray, 'statisticId', 14,21);
-		};
-	}])
-	
-	.filter('personStats', ['$filter', function($filter) {
-		return function(inputArray){ 
-			return $filter('byRange')(inputArray, 'statisticId', 0,15);
-		};
-	}])
-	
-	
-	//This filter allows you to slice an array from a given starting point. 
-	.filter('startFrom', function () {
-	return function (input, start) {
-		if (input) {
-			start = +start;
-			return input.slice(start);
-		}
-		return [];
-	};
-});;
+      if (item[fieldName] > min && item[fieldName] < max) {
+        data.push(item);
+      }
+    });
+    return data;
+  };
+}])
+
+.filter('immunizationStats', ['$filter', function ($filter) {
+  return function (inputArray) {
+    return $filter('byRange')(inputArray, 'statisticId', 20, 29);
+  };
+}])
+
+.filter('respPartyStats', ['$filter', function ($filter) {
+  return function (inputArray) {
+    return $filter('byRange')(inputArray, 'statisticId', 14, 21);
+  };
+}])
+
+.filter('personStats', ['$filter', function ($filter) {
+  return function (inputArray) {
+    return $filter('byRange')(inputArray, 'statisticId', 0, 15);
+  };
+}])
+
+.filter('duration', function () {
+  //Returns duration from milliseconds in hh:mm:ss format.
+  return function (millseconds) {
+    var seconds = Math.floor(millseconds / 1000);
+    var h = 3600;
+    var m = 60;
+    var hours = Math.floor(seconds / h);
+    var minutes = Math.floor((seconds % h) / m);
+    var scnds = Math.floor((seconds % m));
+    var timeString = '';
+    if (scnds < 10) {
+      scnds = "0" + scnds;
+    }
+    if (hours < 10) {
+      hours = "0" + hours;
+    }
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+
+    timeString =
+        (hours > '00' ? (hours + ' h ') : '') +
+        (minutes > '00' ? (minutes + ' m ') : '') +
+        (scnds > '00' ? (scnds + ' s') : '');
+
+    if (!timeString) {
+      return millseconds + ' ms';
+    }
+    return (timeString)// === '' ? (milliseconds + ' ms') : timeString );
+  }
+})
+
+
+//This filter allows you to slice an array from a given starting point.
+.filter('startFrom', function () {
+  return function (input, start) {
+    if (input) {
+      start = +start;
+      return input.slice(start);
+    }
+    return [];
+  };
+});
+;
--- src/main/resources/static/app/service/data.service.js
+++ src/main/resources/static/app/service/data.service.js
@@ -1,182 +1,157 @@
 angular.module('messageHubDemoApp')
 
-	.factory('MessageCountHistoryFactory', function($resource) {
-	    return $resource('provider/:providerKey/counts/year/:year');
-	})
-	
-	.factory('MessageListFactory',	function($resource) {
-		return $resource('messages/:providerKey/date/:date/messages/:messages/page/:page/?filters=:filters');
-	})
-	
-	.factory('MessageDetailFactory', function($resource) {
-		return $resource('messages/:id');
-	})
-	
-	.factory('FacilityList', function($resource) {
-	    return $resource('facilities/');
-	})
+.factory('MessageCountHistoryFactory', function ($resource) {
+  return $resource('provider/:providerKey/counts/year/:year');
+})
 
-    .factory('Hl7JsonPoster', function ($resource) {
-        return $resource('messages/json/notsaved');//posts a json object
-    })
+.factory('MessageListFactory', function ($resource) {
+  return $resource(
+      'messages/:providerKey/date/:date/messages/:messages/page/:page/?filters=:filters');
+})
 
-    .factory('Hl7JsonExample', function ($resource) {
-    	return $resource('messages/json/example');//gets a json object with an example message
-    })
-    
-    .factory('ReportExample', function ($resource) {
-    	return $resource('report/demo');//gets a json object with an example message
-    })
+.factory('MessageDetailFactory', function ($resource) {
+  return $resource('messages/:id');
+})
 
-    .factory('Reporter', function ($resource) {
-        return $resource('report/:providerKey/date/:date');//gets a json object with an example message
-    })
-    
-    .factory('ReportMessage', function ($resource) {
-    	return $resource('report/message');//gets a json object with an example message
-    })
-    
-    .directive('fileModel', ['$parse', function ($parse) {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            var model = $parse(attrs.fileModel);
-            var modelSetter = model.assign;
-            
-            element.bind('change', function(){
-                scope.$apply(function(){
-                    modelSetter(scope, element[0].files[0]);
-                });
-            });
-        }
-    };
+.factory('FacilityList', function ($resource) {
+  return $resource('facilities/');
+})
+
+.factory('Hl7JsonPoster', function ($resource) {
+  return $resource('messages/json/notsaved');//posts a json object
+})
+
+.factory('Hl7JsonExample', function ($resource) {
+  return $resource('messages/json/example');//gets a json object with an example message
+})
+
+.factory('ReportExample', function ($resource) {
+  return $resource('report/demo');//gets a json object with an example message
+})
+
+.factory('Reporter', function ($resource) {
+  return $resource('report/:providerKey/date/:date');//gets a json object with an example message
+})
+
+.factory('ReportMessage', function ($resource) {
+  return $resource('report/message');//gets a json object with an example message
+})
+
+.directive('fileModel', ['$parse', function ($parse) {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      var model = $parse(attrs.fileModel);
+      var modelSetter = model.assign;
+
+      element.bind('change', function () {
+        scope.$apply(function () {
+          modelSetter(scope, element[0].files[0]);
+        });
+      });
+    }
+  };
 }])
 
 .service('fileUpload', ['$http', function ($http) {
-    this.uploadFileToUrl = function(file){
-        var fd = new FormData();
-        fd.append('file', file);
+  this.uploadFileToUrl = function (file) {
+    var fd = new FormData();
+    fd.append('file', file);
 
-        return $http.post("file/upload-messages", fd, {
-            transformRequest: angular.identity,
-            headers: {'Content-Type': undefined}
-        });
-    };
+    return $http.post("file/upload-messages", fd, {
+      transformRequest: angular.identity,
+      headers: {'Content-Type': undefined}
+    });
+  };
 
-    this.initiateFileProcess = function(fileId){
-        var fd = new FormData();
-        fd.append('fileId', fileId);
-        return $http.post("file/process-file",fd, {
-            transformRequest: angular.identity,
-            headers: {'Content-Type': undefined}
-        });
-    };
+  this.initiateFileProcess = function (fileId) {
+    var fd = new FormData();
+    fd.append('fileId', fileId);
+    return $http.post("file/process-file", fd, {
+      transformRequest: angular.identity,
+      headers: {'Content-Type': undefined}
+    });
+  };
 
-    this.reportFileProcess = function(fileId){
-        return $http.get("file/report-file?fileId="+fileId);
-    };
+  this.reportFileProcess = function (fileId) {
+    return $http.get("file/report-file?fileId=" + fileId);
+  };
 
-    this.pauseFileProcess = function(fileId){
-        return $http.get("file/stop-file?fileId="+fileId);
-    };
+  this.pauseFileProcess = function (fileId) {
+    return $http.get("file/stop-file?fileId=" + fileId);
+  };
 
-    this.unpauseFileProcess = function(fileId){
-        return $http.get("file/unpause-file?fileId="+fileId);
-    };
+  this.unpauseFileProcess = function (fileId) {
+    return $http.get("file/unpause-file?fileId=" + fileId);
+  };
 
-    this.getAcks = function(fileId){
-        return $http.get("file/report-acks?fileId="+fileId);
-    };
-    this.removeFile = function(fileId){
-        return $http.get("file/remove-file?fileId="+fileId);
-    };
-    this.getQueues = function(){
-        return $http.get("file/get-queues");
-    };
+  this.getAcks = function (fileId) {
+    return $http.get("file/report-acks?fileId=" + fileId);
+  };
+  this.removeFile = function (fileId) {
+    return $http.get("file/remove-file?fileId=" + fileId);
+  };
+  this.getQueues = function () {
+    return $http.get("file/get-queues");
+  };
 }])
 
-
-    .factory('MessageResult', function ($resource) {
-    	return $resource('messages/msg-result');//gets a string with ack info
-    })
-    
-    .factory('MessageDownload', function ($resource) {
-    	return $resource('messages/msg-download');//gets a string with ack info
-    })
-
-    .factory('Coder', function ($resource) {
-        return $resource('codes/:providerKey/:dateStart/:dateEnd');//gets a collection of codes from the provider
-    })
-
-    .factory('VaccineCodes', function ($resource) {
-        return $resource('codes/vaccinations/:providerKey/:dateStart/:dateEnd');//gets a collection of codes from the provider
-    })
-
-	.directive('fileModel', ['$parse', function ($parse) {
-	    return {
-	        restrict: 'A',
-	        link: function(scope, element, attrs) {
-	            var model = $parse(attrs.fileModel);
-	            var modelSetter = model.assign;
-	            
-	            element.bind('change', function(){
-	                scope.$apply(function(){
-	                    modelSetter(scope, element[0].files[0]);
-	                });
-	            });
-	        }
-	    };
-	}])
-	
-	.filter('newline', function ($sce) {
-	    return function (text) {
-	        return text ? $sce.trustAsHtml(text.replace(/\r/g, '<br/>')) : '';
-	    };
-	})
-
-.filter('duration', function() {
-  //Returns duration from milliseconds in hh:mm:ss format.
-  return function(millseconds) {
-    var seconds = Math.floor(millseconds / 1000);
-    var h = 3600;
-    var m = 60;
-    var hours = Math.floor(seconds/h);
-    var minutes = Math.floor( (seconds % h)/m );
-    var scnds = Math.floor( (seconds % m) );
-    var timeString = '';
-    if(scnds < 10) scnds = "0"+scnds;
-    if(hours < 10) hours = "0"+hours;
-    if(minutes < 10) minutes = "0"+minutes;
-
-    timeString =
-        (hours > '00' ? (hours +' h ') : '') +
-        (minutes > '00' ? (minutes +' m ') : '') +
-        (scnds > '00' ? (scnds +' s') : '');
-
-    if (!timeString) {
-      return millseconds + ' ms';
-    }
-    return (timeString)// === '' ? (milliseconds + ' ms') : timeString );
-  }
+.factory('MessageResult', function ($resource) {
+  return $resource('messages/msg-result');//gets a string with ack info
 })
 
-	.factory('SettingNameGetterAndSetter', function ($resource) {
-    	return $resource('settings/name/:settingName');//gets and posts setting information to database
-    })
-    
-	.factory('NistUrlGetterAndSetter', function ($resource) {
-    	return $resource('nist/validator/url');//gets and sets nist validator url
-    })
-    
-	.factory('NistConnectionStatusGetter', function ($resource) {
-    	return $resource('nist/validator/connection');//gets the nist validator connection status
-    })
-    
-	.factory('NistExceptionGetter', function ($resource) {
-    	return $resource('nist/validator/exception');//gets the nist validator exception
-    })
-    
-	.factory('NistClearException', function ($resource) {
-    	return $resource('nist/validator/clear-exception');//clears the nist validator exception
-    })
+.factory('MessageDownload', function ($resource) {
+  return $resource('messages/msg-download');//gets a string with ack info
+})
+
+.factory('Coder', function ($resource) {
+  return $resource('codes/:providerKey/:dateStart/:dateEnd');//gets a collection of codes from the provider
+})
+
+.factory('VaccineCodes', function ($resource) {
+  return $resource('codes/vaccinations/:providerKey/:dateStart/:dateEnd');//gets a collection of codes from the provider
+})
+
+.directive('fileModel', ['$parse', function ($parse) {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      var model = $parse(attrs.fileModel);
+      var modelSetter = model.assign;
+
+      element.bind('change', function () {
+        scope.$apply(function () {
+          modelSetter(scope, element[0].files[0]);
+        });
+      });
+    }
+  };
+}])
+
+.filter('newline', function ($sce) {
+  return function (text) {
+    return text ? $sce.trustAsHtml(text.replace(/\r/g, '<br/>')) : '';
+  };
+})
+
+.factory('SettingNameGetterAndSetter', function ($resource) {
+  return $resource('settings/name/:settingName');//gets and posts setting information to database
+})
+
+.factory('NistUrlGetterAndSetter', function ($resource) {
+  return $resource('nist/validator/url');//gets and sets nist validator url
+})
+
+.factory('NistConnectionStatusGetter', function ($resource) {
+  return $resource('nist/validator/connection');//gets the nist validator connection status
+})
+
+.factory('NistExceptionGetter', function ($resource) {
+  return $resource('nist/validator/exception');//gets the nist validator exception
+})
+
+.factory('NistClearException', function ($resource) {
+  return $resource('nist/validator/clear-exception');//clears the nist validator exception
+})
+
 ;
