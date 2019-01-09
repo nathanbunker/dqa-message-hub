085e963ea8e14fd5f5d9e9978f5e747835a5c248
Josh Hull <j.hull@kunzleigh.com>
2019-01-09 16:54:00 -0500

    MQE-98 initial commit.  Scaffolding is set up

---
+++ angular/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {FileUploadComponent} from './file-upload/file-upload.component';
+
+const routes: Routes = [
+  { path: 'file', component: FileUploadComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ angular/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FileUploadComponent } from './file-upload/file-upload.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCoffee, faSlidersH, faStethoscope, faCaretDown } from '@fortawesome/free-solid-svg-icons';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FileUploadComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    NgbModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+  constructor() {
+  // Add an icon to the library for convenient access in other components
+  library.add(faSlidersH, faCoffee, faStethoscope, faCaretDown);
+  }
+}
