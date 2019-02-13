a22750a7dc48fe8af56a763e9f543d6b324646d4
Josh Hull <j.hull@kunzleigh.com>
2019-02-13 10:30:25 -0500

    MQE-98 Component Architecture

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {FileUploadComponent} from './file-upload/file-upload.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {MessageDetailComponent} from './message-detail/message-detail.component';
+
 
 const routes: Routes = [
   { path: 'file', component: FileUploadComponent }
+  ,  { path: 'dashboard', component: DashboardComponent }
+  ,  { path: 'messages', component: MessageDetailComponent }
 ];
 
 @NgModule({
