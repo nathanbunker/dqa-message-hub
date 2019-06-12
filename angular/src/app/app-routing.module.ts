d4e0d4efcaed174503d26ef0f8066b7af6e9ee97
Josh Hull <j.hull@kunzleigh.com>
2019-06-12 16:14:38 -0400

    MQE-98 converting demo page

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import {FileUploadComponent} from './file-upload/file-upload.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {MessageDetailComponent} from './message-detail/message-detail.component';
+import {MessageValidationComponent} from './message-validation/message-validation.component';
 import {SettingsComponent} from './settings/settings.component';
 import { DetectionDocComponent } from './documentation/detection-doc/detection-doc.component';
 
@@ -12,6 +13,7 @@ const routes: Routes = [
   ,  { path: 'dashboard', component: DashboardComponent }
   ,  { path: 'detections', component: DetectionDocComponent }
   ,  { path: 'messages', component: MessageDetailComponent }
+  ,  { path: 'validation', component: MessageValidationComponent }
   ,  { path: 'settings', component: SettingsComponent }
 ];
 
